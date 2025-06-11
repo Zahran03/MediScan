fcb4be46ba7072049a12c3c741328305eb72923d
Tiwi01 <161281153+Pertiwi0101@users.noreply.github.com>
2025-06-11 13:13:35 +0700

    Button.js

---
+++ components/Button.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Button.module.css';
+
+export default function Button({ text, onClick }) {
+  return (
+    <button className={styles.button} onClick={onClick}>
+      {text}
+    </button>
+  );
+}
